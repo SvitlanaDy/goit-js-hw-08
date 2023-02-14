ec99b7e5b87acb47bf083a7a422ec5eae4c96713
Svitlana Dyshkant <110805508+SvitlanaDy@users.noreply.github.com>
2023-02-14 22:52:31 +0700

    ex1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const imagesElement = document.querySelector(".gallery");
+
+const gallery = galleryItems
+  .map((img) => {
+    return `<a class="gallery__item" href=${img.original}>
+                <img 
+                class="gallery__image"
+                src=${img.preview} 
+                alt=${img.description}>
+            </a>`;
+  })
+  .join("");
+
+  imagesElement.insertAdjacentHTML("afterbegin", gallery);
+  
+
+new SimpleLightbox('.gallery a', {
+    captionData: 'alt',
+    captionDelay: 250,
+});
 
 console.log(galleryItems);
