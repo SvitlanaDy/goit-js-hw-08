96262e8f3d8ce5d2b0ef8eb01ceb3f95b261fc0a
Svitlana Dyshkant <110805508+SvitlanaDy@users.noreply.github.com>
2023-02-19 16:02:13 +0700

    upd 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,7 +6,8 @@ const email = document.querySelector('input');
 const STORAGE_KEY = 'feedback-form-state';
 const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
-fillForm();
+// fillForm();
+fillTextField();
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input',throttle(onFormInput, 500));
@@ -16,17 +17,24 @@ function onFormInput(evt){
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
   console.log(evt.target.value);
 }
-function fillForm(){
+function fillTextField(){
     const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    // if (savedMessage){
+    // textarea.value = savedMessage.message;
+    // email.value = savedMessage.email;
+    // }
+   
     if (savedMessage){
-    textarea.value = savedMessage.message;
-    email.value = savedMessage.email;
-    }}
+        form.message.value = formData.message || '';
+        form.email.value = formData.email || '';  
+    }
+  
+}
 
 function onFormSubmit(evt) {
     evt.preventDefault();
    
     evt.target.reset();
     localStorage.removeItem(STORAGE_KEY);
-    // console.log(formData);
+    console.log(formData);
 }
